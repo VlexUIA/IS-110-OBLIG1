935c96a4d12a5a30b98f774c841ae4a38b1cc4c9
VlexUIA <xfalkon61@gmail.com>
2026-03-15 15:03:46 +0100

    Ansatt.cs (Ferdig)

--- Ansatt.cs
+++ Ansatt.cs
@@ -1,1 +1,22 @@
-Ansatt.cs
+namespace IS110OBLIG1
+{
+    class Ansatt
+    {
+        // Informerer oss om Ansatt - ID, Navn og stilling. 
+        public string AnsattId { get; set; }
+        public string Navn { get; set; }
+        public string Stilling { get; set; }
+
+        public Ansatt(string id, string navn, string stilling)
+        {
+            AnsattId = id;
+            Navn = navn;
+            Stilling = stilling;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Ansatt: " + Navn + " | ID: " + AnsattId + " | Stilling: " + Stilling);
+        }
+    }
+}

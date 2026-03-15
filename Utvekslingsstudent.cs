14b6632fe9a852e60c4d46e3564475cc97541b17
VlexUIA <xfalkon61@gmail.com>
2026-03-15 15:03:22 +0100

    Utvekslingsstudent.cs (Ferdig)

--- Utvekslingsstudent.cs
+++ Utvekslingsstudent.cs
@@ -1,1 +1,25 @@
-Utvekslingsstudent.cs
+namespace IS110OBLIG1
+{
+    // Arver fra Student - får med seg alt Student har
+    class Utvekslingsstudent : Student
+    {
+        public string Hjemuniversitet { get; set; }
+        public string Land { get; set; }
+        public string Periode { get; set; }
+
+        public Utvekslingsstudent(string id, string navn, string epost, string hjemuniversitet, string land, string periode)
+            : base(id, navn, epost) // kaller konstruktøren til Student
+        {
+            Hjemuniversitet = hjemuniversitet;
+            Land = land;
+            Periode = periode;
+        }
+
+        // override betyr at vi overskriver PrintInfo() fra Student
+        public override void PrintInfo()
+        {
+            base.PrintInfo(); // skriver ut det Student sin PrintInfo() skriver
+            Console.WriteLine("  Hjemuniversitet: " + Hjemuniversitet + " | Land: " + Land + " | Periode: " + Periode);
+        }
+    }
+}
